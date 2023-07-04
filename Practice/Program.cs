0b67fcb6a68f113944277b232652ebf8dd84a646
me <me@DESKTOP-4RS4N30>
2023-07-04 11:28:33 +0500

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Anand";
+            Console.WriteLine(" Hello Mr.  " + name1);
+            Console.WriteLine(5 + 5);
+
+            //Here we are steping towards comments( this is single line comment )
+            /* this is the multiline comment
+             hanjhro
+             anand 
+            */
+
+            int a = 15; // integer value have been assigned to variable a
+            string name = " Anand "; // we have declared the value " anand " to name in data type string.
+
+            Console.WriteLine(name);
+            Console.WriteLine(a);    
+
+            Console.WriteLine(name + a); // here we are getting both data types values together in one line 
+
+
+            //also we can assign and get values like this
+
+            int number;
+            number = 34;
+            Console.WriteLine(number);
+
+            
+        Console.ReadLine();
+            
+        }
+
+
+    }
+}
---
+++ Practice/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Vehicle
+    {
+        public string Brand = " FORD";
+        public void Honk()
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
